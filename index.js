80522bc15b736c0cac250b1d7664c79fc3911f5c
Guilherme <guilermedias.gd@gmail.com>
2024-06-03 20:40:15 -0300

    “Update”

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// elementos DOM aqui eu eu dei a funçao aos botons criados no html, tem que fazer com muita calma, extremamente confuso//
+
 let editButton = document.querySelector(".profile__edit-button");
 let closeButton = document.querySelector(".popup__container__close-button");
 let formElement = document.querySelector(".popup__container");
@@ -15,22 +17,27 @@ function closePopUp() {
 }
 
 function handleProfileFormSubmit(evt) {
-  evt.preventDefault();
+  evt.preventDefault(); //evita o recarregamento da página//
 
+  // aqui memoriza os dados que digitei //
   let nameInput = document.querySelector("#name").value;
   let jobInput = document.querySelector("#job").value;
 
+  // vai imprimir os dados, porem precisa ligar//
   console.log(nameInput, jobInput);
 
+  //local onde será exibido //
   let userName = document.querySelector(".profile__name");
   let userJob = document.querySelector(".profile__job");
 
+  // define os lugares que vai imprimir o nome e trabalho, subistituindo ooque tinha antes//
   userName.textContent = nameInput;
   userJob.textContent = jobInput;
 
-  closePopUp();
+  closePopUp(); //fecha o formulario//
 }
 
+// quando o botão open é clicado ele puxa a função openPopUp e abre o formulário //
 editButton.addEventListener("click", openPopUp);
 closeButton.addEventListener("click", closePopUp);
 formElement.addEventListener("submit", handleProfileFormSubmit);
