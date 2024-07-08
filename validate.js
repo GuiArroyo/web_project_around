e8ac8cb70813baa1b20c5c4ab31dc3bff6152ae0
Guilherme <guilermedias.gd@gmail.com>
2024-07-08 18:37:22 -0300

    “Update”

--- validate.js
+++ validate.js
@@ -1,8 +1,29 @@
 const showInputError = (formElement, inputElement, errorMessage, settings) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.add(settings.inputErrorClass);
-  // Função para mostrar erro de input, adicionando mensagem de erro e classe de erro ao input não possui erro no site, se digitar um carcter só mostra o erro certo
-  // Esta tudo funcionando, mostra cada erro de acordo, não to entendendo, até no coolerning bugaram, porque funciona.
+  
+
+    // Verifica a propriedade ValidityState e define a mensagem de erro apropriada
+    if (inputElement.validity.valueMissing) {
+      errorMessage = 'Este campo é obrigatório.';
+    } else if (inputElement.validity.typeMismatch) {
+      errorMessage = 'Por favor, insira um valor no formato correto.';
+    } else if (inputElement.validity.patternMismatch) {
+      errorMessage = 'O valor inserido não corresponde ao padrão esperado.';
+    } else if (inputElement.validity.tooShort) {
+      errorMessage = `O valor deve ter pelo menos ${inputElement.minLength} caracteres; você inseriu ${inputElement.value.length}.`;
+    } else if (inputElement.validity.tooLong) {
+      errorMessage = `O valor deve ter no máximo ${inputElement.maxLength} caracteres; você inseriu ${inputElement.value.length}.`;
+    } else if (inputElement.validity.rangeUnderflow) {
+      errorMessage = `O valor deve ser maior ou igual a ${inputElement.min}.`;
+    } else if (inputElement.validity.rangeOverflow) {
+      errorMessage = `O valor deve ser menor ou igual a ${inputElement.max}.`;
+    } else if (inputElement.validity.stepMismatch) {
+      errorMessage = 'Por favor, insira um valor válido.';
+    } else if (inputElement.validity.customError) {
+      errorMessage = errorMessage; // Use a mensagem de erro personalizada passada como argumento
+    }
+    
   errorElement.textContent = errorMessage;
 };
 
